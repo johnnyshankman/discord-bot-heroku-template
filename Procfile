ce10f167bde5f53add087ba092b35ca4c06ebd90
Johnny Shankman`` <johnnyshankman@users.noreply.github.com>
2024-08-12 17:49:27 -0400

    convert to using typescript with ts-node

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-worker: npm start
+worker: npm run start
 
--- index.ts
+++ index.ts
@@ -4,38 +4,52 @@ import { Routes } from 'discord-api-types/v9'
 import { Client, Intents, MessageEmbed } from 'discord.js'
 import { SlashCommandBuilder } from '@discordjs/builders'
 
-// Load the config file with the bot's token, client ID, and guild ID
+/**
+ * @dev Load the config file with the bot's token, client ID, and guild ID
+ */
 const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
 const { clientId, guildId, token } = config
 
-// Create a new client instance and set the intents
+/**
+ * @dev Create a new Discord client instance and set the intents.
+ * The intents are used to specify what events the bot will listen to.
+ */
 const client = new Client({ intents:
   [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 })
 
-// Setup the list of available commands
+/**
+ * @dev Configure the commands that the bot will listen to.
+ */
 const commands = [
   new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
   new SlashCommandBuilder().setName('test').setDescription('Replies with a test embed!'),
 ].map(command => command.toJSON())
 
-// Setup REST API
+/**
+ * @dev Setup the REST API to register the commands.
+ */
 const rest = new REST({ version: '9' }).setToken(token)
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
   .then(() => console.log('Successfully registered Discord Bot application commands.'))
   .catch(console.error)
 
-
-// HANDLE ALL COMMANDS HERE
-client.on('interactionCreate', async interaction => {
+/**
+ * @dev Listen for Command-type interactions with the bot.
+ */
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) {
+    return
+  }
+
+  if (!client.user) {
     return
   }
 
   const { commandName } = interaction
 
-  const interactionUser = await interaction.guild.members.fetch(interaction.user.id)
-  console.log(`User Interacted: ${interactionUser.user.username}#${interactionUser.user.discriminator} (${interactionUser.user.id})`)
+  const interactionUser = await interaction?.guild?.members.fetch(interaction.user.id)
+  console.log(`User Interacted: ${interactionUser?.user.username}#${interactionUser?.user.discriminator} (${interactionUser?.user.id})`)
 
   if (commandName === 'ping') {
     client.user.setActivity(`Pong!`)
@@ -60,15 +74,23 @@ client.on('interactionCreate', async interaction => {
 })
 
 /**
- * When the client is fully ready, this event will trigger.
- * This is the best time to set bot status and activity that it's working.
- * It's also a good place to setup intervals that check for interactions.
+ * @dev When the client is fully ready, this event will trigger.
+ * @note This is the best time to set initial bot status and log that the bot is ready.
+ * @important This is the best place to setup intervals that check for interactions.
  */
 client.once('ready', async () => {
   console.log('Starting Discord Bot...')
-  client.user.setStatus('available')
+
+  if (!client.user) {
+    console.error('Client user is not available.')
+    return
+  }
+
+  client.user.setStatus('online')
   client.user.setActivity('Hello World')
+
+  console.log(`Discord Bot is ready as ${client.user.tag}!`)
 })
 
-// Login to Discord with your client's token
+// Login to Discord client token
 client.login(token)
